1f3049843f7f0ead3bf05e99a5e715f4cfb92318
Reynaldo Layme <layme.reynaldo@icloud.com>
2020-04-04 22:07:17 -0300

    feat(notifications): add styles and based structure

--- src/components/Notifications/index.js
+++ src/components/Notifications/index.js
@@ -1,7 +1,51 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { MdNotifications } from 'react-icons/md';
+
+import {
+  Container,
+  Badge,
+  NotificationList,
+  Scroll,
+  Notification,
+} from './styles';
 
 export default function Notifications() {
-  return <div />;
+  return (
+    <Container>
+      <Badge hasUnread>
+        <MdNotifications color="#7159c1" size={20} />
+      </Badge>
+
+      <NotificationList>
+        <Scroll>
+          <Notification unread>
+            <p>Voce possue um novo agendamento para amanha</p>
+            <time>ha 2 dias</time>
+            <button type="button">Marcar como lida</button>
+          </Notification>
+          <Notification>
+            <p>Voce possue um novo agendamento para amanha</p>
+            <time>ha 2 dias</time>
+            <button type="button">Marcar como lida</button>
+          </Notification>
+          <Notification>
+            <p>Voce possue um novo agendamento para amanha</p>
+            <time>ha 2 dias</time>
+            <button type="button">Marcar como lida</button>
+          </Notification>
+          <Notification>
+            <p>Voce possue um novo agendamento para amanha</p>
+            <time>ha 2 dias</time>
+            <button type="button">Marcar como lida</button>
+          </Notification>
+          <Notification>
+            <p>Voce possue um novo agendamento para amanha</p>
+            <time>ha 2 dias</time>
+            <button type="button">Marcar como lida</button>
+          </Notification>
+        </Scroll>
+      </NotificationList>
+    </Container>
+  );
 }
